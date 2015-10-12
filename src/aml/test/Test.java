cc5b234d8856431f493a732df71f070e9fe43952
attakkatutto <attakkatutto@yahoo.com>
2015-10-12 19:00:36 +0200

    refactoring.....

--- src/aml/test/Test.java
+++ src/aml/test/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package aml.test;
 
 import aml.weka.MyWekaManager;
 import java.io.*;
