f5c241ab5601503f084fdb625d5ee84fb4e0fd69
attakkatutto <attakkatutto@yahoo.com>
2015-10-14 19:16:25 +0200

    refactoring.....

--- src/aml/test/Test.java
+++ src/aml/test/Test.java
@@ -7,6 +7,8 @@ package aml.test;
 
 import aml.weka.MyWekaManager;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  *
@@ -29,6 +31,12 @@ public class Test {
         double paramValue = 0.5;
         String paramName = "P1";
         File[] folder = new File("." + File.separator + "dbfiles" + File.separator).listFiles();
+        Arrays.sort(folder, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                return o1.getName().compareTo(o2.getName());
+            }            
+        });
         long start = System.currentTimeMillis();
         for (File f : folder){
             if (f.getName().contains("SYNTHETIC")) {
@@ -42,19 +50,19 @@ public class Test {
     }
     
     
-    public static void oldTest(){     
-        double paramValue = 0.5;
-        String paramName = "P1";
-        File[] folder = new File("." + File.separator + "dbfiles" + File.separator).listFiles();
-        long start = System.currentTimeMillis();
-        for (File f : folder){
-            if (f.getName().contains("SYNTHETIC")) {
-            MyWekaManager m = new MyWekaManager(1);
-            m.calculateResultsOld(f,paramName, paramValue);
-            paramValue = paramValue + 0.05;
-            }
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(" - time elapsed (msec): " + (end - start));
-    }
+//    public static void oldTest(){     
+//        double paramValue = 0.5;
+//        String paramName = "P1";
+//        File[] folder = new File("." + File.separator + "dbfiles" + File.separator).listFiles();
+//        long start = System.currentTimeMillis();
+//        for (File f : folder){
+//            if (f.getName().contains("SYNTHETIC")) {
+//            MyWekaManager m = new MyWekaManager(1);
+//            m.calculateResultsOld(f,paramName, paramValue);
+//            paramValue = paramValue + 0.05;
+//            }
+//        }
+//        long end = System.currentTimeMillis();
+//        System.out.println(" - time elapsed (msec): " + (end - start));
+//    }
 }
